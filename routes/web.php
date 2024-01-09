1f49b79d072d8cf0ad6f259c9b9bb17a42599e03
BaMamadou12 <mamadou.ba2@univ-thies.sn>
2024-01-09 03:34:06 +0000

    J'ai ajouter des controller et en plus j'ai effectué des modifications au niveau du fichier web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccueilController;
+use App\Http\Controllers\AproposController;
+use App\Http\Controllers\HopitalsController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\MedecinsController;
+use App\Http\Controllers\PatientsController;
+use App\Http\Controllers\PharmaciesController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +20,26 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// juste des propostions mai forcement il y'aura des modifications  a effectué
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AccueilController::class ,'acceuil'])->name('accueil');
+
+Route::get('/Hopital', [HopitalsController::class ,'hopital'])->name('hopital');
+
+Route::get('/Pharmacie', [PharmaciesController::class ,'pharmacie'])->name('pharmacie');
+
+Route::get('/Apropos', [AproposController::class ,'apropos'])->name('apropos');
+
+Route::get('/Connexion', [LoginController::class ,'login'])->name('connexion');
+
+Route::get('/Inscription', [RegisterController::class ,'register'])->name('inscription');
+
+Route::get('/Patient', [PatientsController::class ,'patient'])->name('patient');
+
+Route::get('/Medecin', [MedecinsController::class ,'medecin'])->name('medecin');
+
+
+
+
+
+
