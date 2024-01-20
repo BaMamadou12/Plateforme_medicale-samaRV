481ec10760ff5ce26c4fe521b24ea72baf2d3bb4
Ahmada_ <amethgaye1401@gmail.com>
2024-01-20 23:43:13 +0000

    ficher css ajouté

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,8 +9,61 @@
     <title>Plateforme De Prise De rendez-vous </title>
 </head>
 <body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+<header>
+    <div class="logoSante">
+        <a href="" >
+            <img src="images/logo_sante.png" alt="logo santé">
+            <h2>SAMA RV</h2>
+        </a>
+    </div>
+    <nav>
+        <ul>
+            <li>
+                <a href="">Accueil</a>
+            </li>
+            <li>
+                <a href="">Hôpitaux</a>
+            </li>
+            <li>
+                <a href="">Pharmacies</a>
+            </li>
+            <li>
+                <a href="">A propos</a>
+            </li>
+
+        </ul>
+    </nav>
+
+    <div class="auth">
+        <a href="">Connexion</a>
+        <a href="">Inscription</a>
+        <!--            <a href="">mamadou</a>-->
+        <!--            <a href="">déconnexion</a>-->
+    </div>
+</header>
+<main>
+    <section class="sect-1">
+        <div>
+            <div class="op"></div>
+            <img src="images/hdd.jpg" alt="">
+        </div>
+
+        <div class="container">
+            <h2>Bienvenu Dans Votre Plateforme De Prise de Rendez-vous</h2>
+            <!--                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>-->
+
+            <form action="#" method="get">
+                <input type="text" name="" id="" placeholder="saisir un hôpital ou pharmacie">
+                <button type="submit">Rechercher</button>
+            </form>
+        </div>
+    </section>
+</main>
+
+<footer>
+
+</footer>
+
+
 </body>
 </html>
