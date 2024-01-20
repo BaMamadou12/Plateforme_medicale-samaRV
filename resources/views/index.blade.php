867b2924d2f43be8519a232aa999e2da52e332c9
Ahmada_ <amethgaye1401@gmail.com>
2024-01-20 18:42:31 +0000

    tailwindCss intégré

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    @vite('resources/css/app.css')
+    <title>Plateforme De Prise De rendez-vous </title>
 </head>
 <body>
-    hello
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
 </body>
 </html>
