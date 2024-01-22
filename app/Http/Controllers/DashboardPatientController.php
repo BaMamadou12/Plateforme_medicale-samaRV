9d8debe22bb06b21c5571eb236eb78de43ec8b0b
Ahmada_ <amethgaye1401@gmail.com>
2024-01-22 16:25:30 +0000

    design du tableau de bord patient

--- app/Http/Controllers/DashboardPatientController.php
+++ app/Http/Controllers/DashboardPatientController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class DashboardPatientController extends Controller
 {
     public function index(){
-        return view('dashboard');
+        return view('patient.dashboard');
     }
 }
