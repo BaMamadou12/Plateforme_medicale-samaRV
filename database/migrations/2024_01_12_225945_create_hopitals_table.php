d4b6255ba33f365e10b29ac1e934eff7e19d331d
BaMamadou12 <mamadou.ba2@univ-thies.sn>
2024-01-27 14:53:48 +0000

    ajout d'une cle etrangere dans la table hopitals

--- database/migrations/2024_01_12_225945_create_hopitals_table.php
+++ database/migrations/2024_01_12_225945_create_hopitals_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('email');
             $table->string('ville');
             $table->string('telephone');
+            $table->unsignedBigInteger('id_medecin');
             $table->timestamps();
         });
     }
