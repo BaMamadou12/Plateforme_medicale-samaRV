4052d1b68ea55621082f8a2bda23ebda6e059942
BaMamadou12 <mamadou.ba2@univ-thies.sn>
2024-02-05 23:41:22 +0000

    Ajout pour plus de securité sur la redirection au niveeau de l'url

--- app/Http/Controllers/MedecinsController.php
+++ app/Http/Controllers/MedecinsController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Medecin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 
 class MedecinsController extends Controller
 {
-
-
+    public function __construct()
+    {
+        $this->middleware('medecin');
+    }
 
     public  function medecin(Request $request):View{
 
